1e005cdc428e0b16432c024d3b905798adc176f5
Glynn Bird <glynn.bird@gmail.com>
2020-05-18 14:07:50 +0100

    invalid letters

--- app.js
+++ app.js
@@ -4,22 +4,39 @@ var app = new Vue({
   el: '#app',
   data: {
     source: 'Saoirse Ronan',
-    target: ''
+    target: '',
+    error: ''
   },
   methods: {
   },
   computed: {
     left : function() {
       let str = this.source
+      const invalidLetters = []
       for(var i in this.target) {
         const letter = this.target[i]
         if (letter !== ' ') {
           const re = new RegExp(letter, 'i')
-          str = str.replace(re, '')
+          if (str.match(re)) {
+            str = str.replace(re, '')
+          } else {
+            invalidLetters.push(letter)
+          }
+
         }
+      }
+      if (invalidLetters.length > 0) {
+        this.error = 'Invalid letters: ' + invalidLetters.join(' ')
+      } else {
+        this.error = ''
       }
       str = str.toLowerCase().split('').sort().join('')
       return str
+    },
+    messageClass () {
+      return {
+        'md-invalid': (this.error.length > 0)
+      }
     }
   }
 })
